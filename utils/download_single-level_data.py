17c5491668ba8baa60df27fe72aeb9ca6fc496c2
HongjinZhu <hz2291@nyu.edu>
2025-08-26 17:19:26 -0400

    Expanded analyses to multiple sites

--- utils/download_single-level_data.py
+++ utils/download_single-level_data.py
@@ -6,8 +6,8 @@ request = {
     "variable": [
         "surface_solar_radiation_downwards"
     ],
-    "year": ["2022"],
-    "month": ["01", "02"],  # Jan + Feb 2022
+    "year": ["2021"],
+    "month": ["07", "08"],  # Jul + Aug 2021
     "day": [
         "01", "02", "03", "04", "05", "06", "07",
         "08", "09", "10", "11", "12", "13", "14",
